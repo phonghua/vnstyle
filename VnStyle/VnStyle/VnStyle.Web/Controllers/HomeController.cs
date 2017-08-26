7af895abdbc619a93d6364ec939b977379af87b3
phong.hua <phong.hua@saigontechnology.vn>
2017-08-26 20:37:01 +0700

    test IOC

--- VnStyle/VnStyle/VnStyle.Web/Controllers/HomeController.cs
+++ VnStyle/VnStyle/VnStyle.Web/Controllers/HomeController.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Ricky.Infrastructure.Core.Caching;
+using Ricky.Infrastructure.Core.ObjectContainer;
 
 namespace VnStyle.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ICacheManager _cacheManager;
+
+        public HomeController()
+        {
+            _cacheManager = EngineContext.Current.Resolve<ICacheManager>();
+        }
         public ActionResult Index()
         {
+            
+
             ViewBag.Title = "Home Page";
 
             return View();
