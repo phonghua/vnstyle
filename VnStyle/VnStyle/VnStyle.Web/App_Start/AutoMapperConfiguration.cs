4fe80c78df66085adbd72ed9d962c0731dfba83f
phong.hua <phong.hua@saigontechnology.vn>
2017-09-03 15:27:53 +0700

    added autoMapper

---
+++ VnStyle/VnStyle/VnStyle.Web/App_Start/AutoMapperConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using VnStyle.Services.Business.Dtos;
+using VnStyle.Services.Data.Domain;
+
+namespace VnStyle.Web
+{
+    public static class AutoMapperConfiguration
+    {
+        public static void Initialize()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                //cfg.CreateMap<Category, CategoryDto>();
+                cfg.CreateMap<File, FileDto>();
+                //cfg.CreateMap<Review, ReviewDto>();
+
+                //Review, ReviewDto
+            });
+        }
+    }
+}
