1a1e0f396a86271bf26d8a612ef4afb3e516fb2f
phong.hua <phong.hua@saigontechnology.vn>
2017-09-03 18:48:55 +0700

    add featured image into article detail screen

---
+++ VnStyle/VnStyle/VnStyle.Web/Controllers/Api/Models/ArticleLanguageModelView.cs
@@ -0,0 +1,18 @@
+﻿using VnStyle.Services.Data.Domain;
+
+namespace VnStyle.Web.Controllers.Api.Models
+{
+    public class ArticleLanguageModelView
+    {
+        public long Id { get; set; }
+        public string HeadLine { get; set; }
+        public string Content { get; set; }
+        public string Extract { get; set; }
+
+        public string LanguageId { get; set; }
+        public int MetaTagId { get; set; }
+        public virtual MetaTag MetaTag { get; set; }
+    }
+
+    
+}
---
+++ VnStyle/VnStyle/VnStyle.Web/Controllers/Api/Models/ArticleModelView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VnStyle.Services.Data.Enum;
+
+namespace VnStyle.Web.Controllers.Api.Models
+{
+    public class ArticleModelView
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public DateTime PublishDate { get; set; }
+        public int CreatedBy { get; set; }
+        public EArticleState State { get; set; }
+        public string HeadLine { get; set; }
+        public long? FeatureImageId { get; set; }
+        public ImageModelView FeatureImage { get; set; }
+
+        public virtual IEnumerable<ArticleLanguageModelView> ArticleLanguages { get; set; }
+    }
+}
