bdb52438f31276ed0d62672ccea569149347f052
thanghd960 <huadaithang@gmail.com>
2017-09-03 15:57:59 +0700

    change router, added categories controller

---
+++ VnStyle/VnStyle/VnStyle.Web/Controllers/Api/CategoriesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using VnStyle.Services.Data;
+
+namespace VnStyle.Web.Controllers.Api
+{
+    [RoutePrefix("api/categories")]
+    public class CategoriesController : BaseController
+    {
+        //private readonly IBaseRepository<Category> _articleRepository;
+
+        //[Route("{rootCateId}/query")]
+        //public Task<HttpResponseMessage> Get(int rootCateId)
+        //{
+
+        //}
+    }
+}
