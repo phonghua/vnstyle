d00016380e1b35c457c70da7b9161107572b1771
phong.hua <phong.hua@saigontechnology.vn>
2017-09-28 17:58:44 +0700

    artist

--- VnStyle/VnStyle/Applications/VnStylePortal/src/app/components/+cms/component/artist/artist.component.ts
+++ VnStyle/VnStyle/Applications/VnStylePortal/src/app/components/+cms/component/artist/artist.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ViewChild , ElementRef} from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef , ViewContainerRef} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpService, GalleryService, ArtistService, GeneralService } from '../../../../services';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 
 @Component({
   selector: 'app-artist',
@@ -13,40 +14,52 @@ export class ArtistComponent implements OnInit {
   public rootCateName;
   @ViewChild('fileInput') _elFileInput: ElementRef;
 
-  public get colNum (){
+  public get colNum() {
     const deviceWidth = this.generalService.getDocumentWidth();
-    if(deviceWidth == 'sm')  return 2;
-    if(deviceWidth == 'xs')  return 1;
+    if (deviceWidth == 'sm') return 2;
+    if (deviceWidth == 'xs') return 1;
     return 3;
   }
 
-  public get cols(){
+  public get cols() {
     var arr = [];
-    for(let i = 0; i < this.colNum; i++){
+    for (let i = 0; i < this.colNum; i++) {
       arr.push(i);
     }
     return arr;
   }
 
-  
+
 
   public photoGrid = {
     data: []
   };
 
+  public selectedPhoto = -1;
+
   constructor(private httpService: HttpService, private route: ActivatedRoute,
-    private artistService : ArtistService, private generalService : GeneralService) { }
+    private artistService: ArtistService, private generalService: GeneralService,
+    public toastr: ToastsManager, vcr: ViewContainerRef) {
+      this.toastr.setRootViewContainerRef(vcr);
+    }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
-      
-        this.artistId = params["id"];
-        // this.rootCateName = params["rootCateName"];
-  
-        this.artistService.getPhoto(this.artistId).subscribe(data => {
-          this.photoGrid.data = data;
-        });
-      });
+      this.artistId = params["id"];
+      // this.rootCateName = params["rootCateName"];
+
+      this.loadPhoto();
+    });
+  }
+
+  loadPhoto () {
+    this.artistService.getPhoto(this.artistId).subscribe(data => {
+      this.photoGrid.data = data;
+
+      if (this.photoGrid.data && this.photoGrid.data.length > 0) {
+        this.selectedPhoto = 0;
+      }
+    });
   }
 
   browerFiles() {
@@ -60,9 +73,11 @@ export class ArtistComponent implements OnInit {
         console.log("onProgress", processEvent);
       },
       onFinished: (result) => {
-        var imageUrl = result.data.images[0].fileUrl;
-        var imageId = result.data.images[0].id;
+        // var imageUrl = result.data.images[0].fileUrl;
+        // var imageId = result.data.images[0].id;
 
+        this.toastr.success("Upload photo success");
+        this.loadPhoto();
 
       },
       error: () => { }
@@ -72,4 +87,15 @@ export class ArtistComponent implements OnInit {
 
   }
 
+  viewPrevPhoto() {
+    if (this.selectedPhoto > 0) {
+      this.selectedPhoto = this.selectedPhoto - 1;
+    }
+  }
+
+  viewNextPhoto() {
+    if (this.selectedPhoto < this.photoGrid.data.length - 1) {
+      this.selectedPhoto = this.selectedPhoto + 1;
+    }
+  }
 }
