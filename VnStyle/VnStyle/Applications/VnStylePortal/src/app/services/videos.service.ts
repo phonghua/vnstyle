6676aacdb59f78b8be495e637b215a57f7239bc8
phong.hua <phong.hua@saigontechnology.vn>
2017-09-30 17:05:27 +0700

    added video service

---
+++ VnStyle/VnStyle/Applications/VnStylePortal/src/app/services/videos.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpService } from './http.service';
+import { Observable } from 'rxjs/Observable';
+import { SettingsService } from './settings.service';
+
+
+import 'rxjs/Rx';
+@Injectable()
+export class VideosService {
+
+  constructor(private httpService: HttpService, private settingService: SettingsService) { }
+
+  getVideos(): Observable<any> {
+    return this.httpService.get(this.settingService.portal + `api/videos`)
+      .map(res => res.json())
+      .catch(err => Observable.throw(err));
+  }
+
+  postVideo(video): Observable<any> {
+    return this.httpService.post(this.settingService.portal + `api/videos`, video)
+      .catch(err => Observable.throw(err));
+  }
+
+  putVideo(video) : Observable<any>{
+    return this.httpService.put(this.settingService.portal + `api/videos`, video)
+    .catch(err => Observable.throw(err));
+  }
+
+  deleteVideo(id) : Observable<any>{
+    return this.httpService.delete(this.settingService.portal + `api/videos/${id}`)
+    .catch(err => Observable.throw(err));
+  }
+}
