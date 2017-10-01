418f1d1840f1585aa138d1ecdc72fddd03efda70
phong.hua <phong.hua@saigontechnology.vn>
2017-10-01 10:02:20 +0700

    .

--- VnStyle/VnStyle/Applications/VnStylePortal/src/app/components/+cms/component/videos/videos.component.ts
+++ VnStyle/VnStyle/Applications/VnStylePortal/src/app/components/+cms/component/videos/videos.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Input, ViewContainerRef } from '@angular/core';
 import { VideosService } from '../../../../services';
+import { ModalComponent } from 'ng2-bs3-modal/ng2-bs3-modal';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
+import { ConfirmModalComponent } from '../../../shared/confirm-modal/confirm-modal.component';
 
 @Component({
   selector: 'app-videos',
@@ -9,10 +12,18 @@ import { VideosService } from '../../../../services';
 export class VideosComponent implements OnInit {
 
   public videoGrid = {
-    data : [],
-    loading : false
+    data: [],
+    loading: false
   }
-  constructor(private videoService: VideosService) { }
+
+  public videoModel = { title: "", link: "" };
+
+  @ViewChild('modal') modal: ModalComponent;
+  @ViewChild('confirmModal') confirmModal: ConfirmModalComponent;
+
+  constructor(private videoService: VideosService, public toastr: ToastsManager, vcr: ViewContainerRef) {
+    this.toastr.setRootViewContainerRef(vcr);
+  }
 
   ngOnInit() {
     this.loadVideos();
@@ -27,7 +38,35 @@ export class VideosComponent implements OnInit {
     });
   }
 
-  openAddNewForm (){
-    
+  openAddNewForm() {
+    this.videoModel = { title: "", link: "" };
+    this.modal.open();
+  }
+
+  close() {
+    this.modal.close();
+  }
+
+
+
+  ok() {
+    this.videoService.postVideo(this.videoModel).subscribe(() => {
+      this.loadVideos();
+      this.modal.close();
+      this.toastr.success("Thêm mới video thành công");
+    });
+  }
+
+
+  deleteVideo(video){
+    this.confirmModal.message = "Bạn có chắc muốn xóa video này?";
+    this.confirmModal.open();
+    this.confirmModal.ok = () => {
+      this.videoService.deleteVideo(video.id).subscribe(() => {
+        this.loadVideos();
+        this.toastr.success("Xóa video thành công");
+        this.confirmModal.close();
+      });
+    }
   }
 }
